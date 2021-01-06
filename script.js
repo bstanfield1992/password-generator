e74b2f9ba031ed890c59cc30159eff8c1ee64f70
Benjamin Stanfield <bstanfield1992@gmail.com>
2021-01-05 21:14:27 -0700

    added password length

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+function generatePassword() {
+  var password = "password not generated";
+  var lengthOfPassword = 0;
+
+  while (lengthOfPassword < 8 || lengthOfPassword > 128) {
+    lengthOfPassword = window.prompt("Please enter how many characters the password should be.")
+    if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+      window.alert("Please enter a length between 8 and 128");
+    }
+  }
+   
+
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
