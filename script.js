b795db0a3fe557a2d67605f57d829f48da2f7ff7
Benjamin Stanfield <bstanfield1992@gmail.com>
2021-01-06 22:38:54 -0700

    completed generator

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Assignment code here
 
+// Coin flip to switch between upper and lower case
+
+function diceRoll (options) {
+  var dice = Math.floor(Math.random() * options);
+return dice;
+}
+
+function coinFlip() {
+  var coin = Math.floor(Math.random() * 2);
+  return coin;
+}
+
 function generatePassword() {
-  var password = "password not generated";
+  var password = "";
   var lengthOfPassword = 0;
+  var numbers = "1234567890";
+  var special = "~!@#$%^&*()_-+=`,./';][<>?";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var possibleChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
   while (lengthOfPassword < 8 || lengthOfPassword > 128) {
     lengthOfPassword = window.prompt("Please enter how many characters the password should be.")
@@ -10,7 +26,33 @@ function generatePassword() {
       window.alert("Please enter a length between 8 and 128");
     }
   }
-   
+
+  var withNumbers = window.confirm("Would you like numbers in your password?");
+  var upperCase = window.confirm("Would you like to include upper case letters?");
+  var specialC = window.confirm("Would you like to include special characters?");
+
+  if (withNumbers) {
+    for(var i = 0; i < numbers.length; i++) {
+      possibleChar.push(numbers[i])
+    }
+  }
+
+  if (upperCase) {
+    for(var i = 0; i < upper.length; i++) {
+      possibleChar.push(upper[i])
+    }
+  }
+
+  if (specialC) {
+    for(var i = 0; i < special.length; i++) {
+      possibleChar.push(special[i])
+    }
+  }
+
+for (var i = 0; i < lengthOfPassword; i++) {
+  var r = Math.floor(Math.random() * possibleChar.length);
+  password = password + possibleChar[r];
+  }
 
   return password;
 }
